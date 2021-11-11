d4e41096faaf3cbdddd380cc533d42bd9ad86c05
GuusvanderVegt <guusvandervegt2001@gmail.com>
2021-11-11 15:23:05 +0100

    Commit

---
+++ src/components/sections/home/HomeIntro.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="home-intro">
+        <div class="home-intro__content">
+            <h1 class="home-intro__title">Hello, my name is Guus</h1>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
