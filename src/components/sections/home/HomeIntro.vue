98ed58636f119afe8b96b5fb5e9757d4728b05e0
GuusvanderVegt <guusvandervegt2001@gmail.com>
2021-11-14 20:27:27 +0100

    Worked on landing page

--- src/components/sections/home/HomeIntro.vue
+++ src/components/sections/home/HomeIntro.vue
@@ -2,6 +2,13 @@
     <section class="home-intro">
         <div class="home-intro__content">
             <h1 class="home-intro__title">Hello, my name is Guus</h1>
+            <h4 class="home-intro__subtitle">
+                I am a full stack web developer
+            </h4>
+            <div class="home-intro__actions">
+                <a href="#" class="button button--link button--dense">CASES</a>
+                <a href="#" class="button button--link">CONTACT ME</a>
+            </div>
         </div>
     </section>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
     purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     darkMode: "class", // or 'media' or 'class'
     theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "1rem",
+                sm: "1.5rem",
+                lg: "2rem",
+            },
+        },
         extend: {
             colors: {
                 "primary-dark": "#0d2438",
